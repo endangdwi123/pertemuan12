e7fefec1feb5bfc01c3b10c6c0adbfdbd80fe3c1
palcomtech <dwikwira9@gmail.com>
2023-06-14 15:12:53 +0700

    bagianhome

---
+++ lib/home.dart
@@ -0,0 +1,113 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+import 'package:http/http.dart' as http;
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  HomeState createState() => HomeState();
+}
+
+class HomeState extends State<Home> {
+  List _get = [];
+
+  final _lightColors = [
+    Colors.amber.shade300,
+    Colors.lightGreen.shade300,
+    Colors.lightBlue.shade300,
+    Colors.orange.shade300,
+    Colors.pinkAccent.shade100,
+    Colors.tealAccent.shade100
+  ];
+
+  @override
+  void initState() {
+    super.initState();
+
+    _getData();
+  }
+
+  Future _getData() async {
+    try {
+      final response =
+          await http.get(Uri.parse("http://192.168.1.27/note_app/list.php"));
+
+      if (response.statusCode == 200) {
+        final data = jsonDecode(response.body);
+
+        setState(() {
+          _get = data;
+        });
+      }
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Note List'),
+      ),
+      body: _get.length != 0
+          ? MasonryGridView.count(
+              crossAxisCount: 2,
+              itemCount: _get.length,
+              itemBuilder: (context, index) {
+                return GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => Edit(
+                                  id: _get[index]['id'],
+                                )));
+                  },
+                  child: Card(
+                    color: _lightColors[index % _lightColors.length],
+                    child: Container(
+                      constraints:
+                          BoxConstraints(minHeight: (index % 2 + 1) * 85),
+                      padding: const EdgeInsets.all(15),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            '${_get[index]['title']}',
+                            style: const TextStyle(
+                              color: Colors.black,
+                              fontSize: 20,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                );
+              },
+            )
+          : const Center(
+              child: Text(
+                "Np Data Available",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20,
+                ),
+              ),
+            ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.black,
+        child: const Icon(Icons.add),
+        onPressed: () {
+          Navigator.push(
+              context, MaterialPageRoute(builder: (context) => const Add()));
+        },
+      ),
+    );
+  }
+}
